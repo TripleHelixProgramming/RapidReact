0cde8a2538cf9c03d849515968817ee069577431
JoshuaAN <treehousejosh@gmail.com>
2022-01-19 09:33:16 -0500

    Finished intake subsystem

--- src/main/java/frc/robot/intake/Intake.java
+++ src/main/java/frc/robot/intake/Intake.java
@@ -4,7 +4,12 @@
 
 package frc.robot.intake;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
 import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ElectricalConstants;
 import frc.robot.Constants.IntakeConstants;
@@ -14,24 +19,32 @@ public class Intake extends SubsystemBase {
                                         ElectricalConstants.kPneumaticHub,
                                         ElectricalConstants.kIntakeDeployPort, 
                                         ElectricalConstants.kIntakeRetractPort);
+  private TalonSRX motor = new TalonSRX(ElectricalConstants.kIntakeRollerPort);
 
   public Intake() {
-
+    motor.configFactoryDefault();
+    motor.setNeutralMode(NeutralMode.Brake);
+    motor.enableVoltageCompensation(true);
+    motor.configVoltageCompSaturation(12);
+    motor.configPeakCurrentDuration(100,0);
+    motor.configPeakCurrentLimit(60,0);
+    motor.configContinuousCurrentLimit(40);
+    motor.enableCurrentLimit(true);
   }
 
   public void deploy() {
-
+    solenoid.set(Value.kForward);
   }
 
   public void retract() {
-
+    solenoid.set(Value.kReverse);
   }
 
   public void rollerIn() {
-
+    motor.set(ControlMode.PercentOutput, IntakeConstants.kRollerSpeed);
   }
 
   public void rollerStop() {
-
+    motor.set(ControlMode.PercentOutput, 0);
   }
 }
