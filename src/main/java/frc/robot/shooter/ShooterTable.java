2b55ebd0cd5145cc14bd52a83bb463b9b693765b
Justin Babilino <jlbabilino@gmail.com>
2022-02-05 13:20:59 -0500

    Made interpolation table for shooter

---
+++ src/main/java/frc/robot/shooter/ShooterTable.java
@@ -0,0 +1,17 @@
+package frc.robot.shooter;
+
+import frc.lib.util.InterpolationTable;
+
+public class ShooterTable {
+
+    public static final InterpolationTable INTERPOLATION_TABLE;
+    
+    static {
+        INTERPOLATION_TABLE = new InterpolationTable(new double[][]{
+            {},
+            {},
+            {},
+            {}
+        });
+    }
+}
