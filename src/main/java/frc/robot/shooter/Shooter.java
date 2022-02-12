6ea02dc56451fc09dbb3dd62bb41f6263668f1f1
Jasen Jacobsen <jasenj1@gmail.com>
2022-02-12 06:27:35 -0500

    Check hood current limit in periodic(). Create counter to track time hood over current.

--- src/main/java/frc/robot/shooter/Shooter.java
+++ src/main/java/frc/robot/shooter/Shooter.java
@@ -23,6 +23,8 @@ public class Shooter extends SubsystemBase {
   public static boolean DOWN = false;
 
   private boolean hoodDirection;
+  int highCurrentCount = 0;
+  int lowCurrentCount = 0;
 
   private double targetVelocity = 0.0; // Target velocity of the shooter.
   private boolean triggerPull = false;
@@ -85,6 +87,8 @@ public class Shooter extends SubsystemBase {
     SmartDashboard.putNumber("Hood Motor Current", hoodMotor.getOutputCurrent());
     SmartDashboard.putNumber("Hood Angle", getHoodAngle());
 
+    checkHoodCurrentLimit();
+
     runTrigger();
   }
 
@@ -95,16 +99,26 @@ public class Shooter extends SubsystemBase {
 
   public boolean checkHoodCurrentLimit() {
     if ( ShooterConstants.kHoodStopCurrentLimit < hoodMotor.getOutputCurrent()) {
-      SmartDashboard.putBoolean("Hood Hard Stop Hit", true);
-      stopHood();
-      // Assume a high current means we are at the bottom? And reset the encoder?
-      
-      if (DOWN == hoodDirection) {
+      highCurrentCount++;
+      if (10 <= highCurrentCount) {
+        SmartDashboard.putBoolean("Hood Hard Stop Hit", true);
+        // Reset accumulator
+        highCurrentCount = 0;
+        stopHood();
+        // Assume a high current means we are at the bottom. And reset the encoder.
         resetHoodAngle();
+        return true;
+      } else {  // high current, but accumulator not full
+        return false;
       }
-      return true;
+    } else { // Within current limit
+      lowCurrentCount++;
+      if (10 <= lowCurrentCount) {
+        highCurrentCount = 0;
+        lowCurrentCount = 0;
+      }
+      return false;
     }
-    else return false;
   }
 
   public void moveHood(boolean direction) {
