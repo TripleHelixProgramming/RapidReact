45205eb241b181844be8120ad25a8240741edd4b
Justin Babilino <jlbabilino@gmail.com>
2022-03-26 15:19:51 -0400

    Fix DIO LEDs

--- src/main/java/frc/robot/status/commands/DIOSwitchStatus.java
+++ src/main/java/frc/robot/status/commands/DIOSwitchStatus.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 public class DIOSwitchStatus extends CommandBase {
     private Status status;
     int dioState = -1;
+    int ledOffset = 10;
     AddressableLEDBuffer buffer = new AddressableLEDBuffer(Status.ADDRESSABLE_LED_COUNT);
 
     public DIOSwitchStatus(int dioState) {
@@ -15,7 +16,7 @@ public class DIOSwitchStatus extends CommandBase {
         this.dioState = dioState;
         addRequirements(status);
     }
-    
+
     @Override
     public boolean runsWhenDisabled() {
         return true;
@@ -31,31 +32,29 @@ public class DIOSwitchStatus extends CommandBase {
 
     @Override
     public void execute() {
-
-        switch (dioState) {
-            case 0:
-                buffer.setLED(0, Color.kDarkOrange);
-                buffer.setLED(1, Color.kDarkOrange);
-            break;
-            case 1:
-                buffer.setLED(2, Color.kOlive);
-                buffer.setLED(3, Color.kOlive);
-            break;
-            case 2:
-                buffer.setLED(4, Color.kDarkGreen);
-                buffer.setLED(5, Color.kDarkGreen);
-            break;
-            case 3:
-                buffer.setLED(6, Color.kDarkBlue);
-                buffer.setLED(7, Color.kDarkBlue);
-            break;
-            case 4:
-                buffer.setLED(8, Color.kDarkGray);
-                buffer.setLED(9, Color.kDarkGray);
-            break;
-            default:
-                buffer.setLED(0, Color.kDarkRed);
-            break;
+        if (dioState < 0) {
+            buffer.setLED(0, Color.kDarkRed);
+        } else {
+            if (dioState >= 0) {
+                buffer.setLED(ledOffset+ 0, Color.kDarkRed);
+                buffer.setLED(ledOffset+ 1, Color.kDarkRed);
+            }
+            if (dioState >= 1) {
+                buffer.setLED(ledOffset+ 2, Color.kOlive);
+                buffer.setLED(ledOffset+ 3, Color.kOlive);
+            }
+            if (dioState >= 2) {
+                buffer.setLED(ledOffset+ 4, Color.kDarkGreen);
+                buffer.setLED(ledOffset+ 5, Color.kDarkGreen);
+            }
+            if (dioState >= 3) {
+                buffer.setLED(ledOffset+ 6, Color.kDarkBlue);
+                buffer.setLED(ledOffset+ 7, Color.kDarkBlue);
+            }
+            if (dioState >= 4) {
+                buffer.setLED(ledOffset+ 8, Color.kDarkGray);
+                buffer.setLED(ledOffset+ 9, Color.kDarkGray);
+            }
         }
         status.setLedData(buffer);
     }
